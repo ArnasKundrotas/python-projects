425fde21b0da101357664883aedd6e8d831d5b97
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2021-08-26 16:06:50 +0300

    escape char MORE

---
+++ silly-escape-char/silly-esc.py
@@ -0,0 +1,29 @@
+splitString = "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
+
+tabString = "1\t2\t3\t4\t5"
+print(tabString)
+
+print("The per shop, what? owner said no no \'e\'s' uh,...he\'s resting.")
+print("again pet shop owner \" No no 'a'a'a'a' hes what\".")
+
+print ("""The per shop owner said "No, no, 'e's uh,...he's resting". """)
+
+anotherString = """This string
+has been split
+a
+a
+a
+"""
+
+print (anotherString)
+
+anotheranotherString = """\
+This string\
+has been split\
+a\
+a\
+a NOT
+"""
+
+print (anotheranotherString)
