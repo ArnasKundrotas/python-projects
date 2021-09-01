e17cf2b8dba740884d0135674bac9577a8857c4d
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2021-09-01 07:02:20 +0300

    basic commit

---
+++ basic-basic-math.py
@@ -0,0 +1,17 @@
+# just coding for the sake of it
+
+print(4+5) # :))
+print(4+5+1) # :))))
+
+print(4-5+1) # :))))
+print(4/5+1) # :))))
+print(4*5+1) # :))))
+
+# exponents
+# law of exponents
+# illustrating with latex
+
+# the law of exponents --> adding exponents possible only when the base is the same
+
+
+
